48cd553e7a217ee96d8680ae8ff6bb4592e294c3
mai-cs50 <maimohamed.1992@yahoo.com>
2025-04-05 09:08:27 +0200

    s

--- src/Assignment5.sol
+++ src/Assignment5.sol
@@ -14,27 +14,41 @@ contract Assignment5 {
 
     // Constructor
     // Initialize `isActive` to true
+    uint public counter;
+    bool public isActive;
+    mapping (address=>string) public userNames;
+    struct User{
+        uint id;
+        string name;
+    }
+    User[] public users;
     constructor() {
         // Fill in the logic
-
+        isActive = true;
     }
 
     // Function to increment the counter
     // This function should:
     // - Be external
     // - Use a for loop to increment `counter` by 1 for 5 iterations
-    function incrementCounter() {
+    function incrementCounter() external {
         // Fill in the logic
-      
+        for (uint i = 0; i < 5; i++) {
+            counter += 1;
+        }
     }
 
     // Function to toggle `isActive`
     // This function should:
     // - Be public
     // - Use an if statement to toggle `isActive` between true and false
-    function toggleActive() {
+    function toggleActive() public {
         // Fill in the logic
-        
+        if (isActive){
+            isActive = false;
+        } else {
+            isActive = true;
+        }
     }
 
     // Function to add a user
@@ -45,9 +59,11 @@ contract Assignment5 {
         // The memory keyword will be explained later
     // - Add a new user to the `users` array
     // - Update the `userNames` mapping with the address and name
-    function addUser() {
+    function addUser(uint id, string memory name, address userAddress) public {
         // Fill in the logic
-
+        User memory user = User(id, name);
+        users.push(user);
+        userNames[userAddress] = name;
         // Hint: you have to use the keyword `memory` to define the struct
             // For example: User memory user
             // This keyword will be explained later
@@ -58,8 +74,9 @@ contract Assignment5 {
     // This function should:
     // - Be public and view
     // - Return the length of the `users` array
-    function getUserCount() {
+    function getUserCount() public view returns (uint) {
         // Fill in the logic
-
+        uint userlength = users.length;
+        return users.length;
     }
 }
